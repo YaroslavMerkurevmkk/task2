dec1291a7551883b5b30ec7a0d1c75d0d547fdd0
yaroslav <ririfnejfjr@gmail.com>
2022-11-26 23:16:57 +0300

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import random
+from random import randint
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -9,6 +9,23 @@ class Ellipse(QMainWindow):
     def __init__(self):
         super(Ellipse, self).__init__()
         uic.loadUi('Ui.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_ellipse(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_ellipse(self, qp):
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        qp.drawEllipse(100, 100, randint(50, 500), randint(50, 500))
 
 
 if __name__ == '__main__':
